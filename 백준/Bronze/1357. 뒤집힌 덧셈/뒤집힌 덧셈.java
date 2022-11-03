99da97665660053532b0840c975534cff1ea8fc4
angiyaa <110073886+angiyaa@users.noreply.github.com>
2022-11-03 15:48:36 +0900

    [Bronze II] Title: 뒤집힌 덧셈, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sbsb = new StringBuilder();
+		
+		int sum = GetRev(br.readLine());
+		
+		bw.write(Integer.parseInt(sbsb.append(sum).reverse().toString()) + "");
+		bw.flush();
+	}
+	
+	public static int GetRev(String str) {
+		StringTokenizer st = new StringTokenizer(str);
+		
+		StringBuilder sbx = new StringBuilder();
+		StringBuilder sby = new StringBuilder();
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		sbx.append(x).reverse();
+		sby.append(y).reverse();
+		
+		int sum = 0; 
+		
+		sum = Integer.parseInt(sbx.toString()) + Integer.parseInt(sby.toString());
+		
+		return sum;
+	}
+}
