52bf4f8d324a23748a2b407c480b7651e7da962a
angiyaa <110073886+angiyaa@users.noreply.github.com>
2022-11-29 15:18:48 +0900

    [Bronze V] Title: 팩토리얼, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int result = 0;
+		
+		if(N < 1) {
+			result = 1;
+		} else {
+			result = N;
+		}
+		
+		while(N-- > 1) {
+			result *= N;
+		}
+		System.out.println(result);
+	}
+}
